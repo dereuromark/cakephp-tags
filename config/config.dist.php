0110d77592d1a4bf62cbaca8e9620972ec346fe7
mscherer <mark.scherer@spryker.com>
2022-12-26 02:08:41 +0100

    Fix CS

--- config/config.dist.php
+++ config/config.dist.php
@@ -1,10 +1,11 @@
 <?php
 // For full list of configs see behavior and helper
+
 return [
 	'Tags' => [
 		'slug' => null, // Auto slug using Text::slug()
 		'strategy' => 'string', // string or array
 		'delimiter' => ',', // separating the tags
 		'separator' => null, // for namespace prefix, e.g.: ':'
-	]
+	],
 ];
