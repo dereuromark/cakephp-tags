88d12d041cd904d82f81c257f377cbd74b57e66b
mscherer <dereuromark@web.de>
2026-03-19 22:12:53 +0100

    Fix routes to use /admin/tags instead of /tags/admin

--- config/routes.php
+++ config/routes.php
@@ -7,28 +7,24 @@ use Cake\Routing\RouteBuilder;
 /**
  * @var \Cake\Routing\RouteBuilder $routes
  */
-$routes->plugin(
-	'Tags',
-	['path' => '/tags'],
-	function (RouteBuilder $routes): void {
-		$routes->prefix('Admin', function (RouteBuilder $routes): void {
-			$routes->setRouteClass(DashedRoute::class);
+$routes->prefix('Admin', function (RouteBuilder $routes): void {
+	$routes->plugin('Tags', ['path' => '/tags'], function (RouteBuilder $routes): void {
+		$routes->setRouteClass(DashedRoute::class);
 
-			// Dashboard
-			$routes->connect('/', ['controller' => 'TagsDashboard', 'action' => 'index']);
+		// Dashboard
+		$routes->connect('/', ['controller' => 'TagsDashboard', 'action' => 'index']);
 
-			// Tags CRUD
-			$routes->connect('/tags', ['controller' => 'Tags', 'action' => 'index']);
-			$routes->connect('/tags/add', ['controller' => 'Tags', 'action' => 'add']);
-			$routes->connect('/tags/edit/{id}', ['controller' => 'Tags', 'action' => 'edit'], ['pass' => ['id']]);
-			$routes->connect('/tags/view/{id}', ['controller' => 'Tags', 'action' => 'view'], ['pass' => ['id']]);
-			$routes->connect('/tags/delete/{id}', ['controller' => 'Tags', 'action' => 'delete'], ['pass' => ['id']]);
+		// Tags CRUD
+		$routes->connect('/tags', ['controller' => 'Tags', 'action' => 'index']);
+		$routes->connect('/tags/add', ['controller' => 'Tags', 'action' => 'add']);
+		$routes->connect('/tags/edit/{id}', ['controller' => 'Tags', 'action' => 'edit'], ['pass' => ['id']]);
+		$routes->connect('/tags/view/{id}', ['controller' => 'Tags', 'action' => 'view'], ['pass' => ['id']]);
+		$routes->connect('/tags/delete/{id}', ['controller' => 'Tags', 'action' => 'delete'], ['pass' => ['id']]);
 
-			// Merge
-			$routes->connect('/tags/merge', ['controller' => 'Tags', 'action' => 'merge']);
-			$routes->connect('/tags/merge-preview', ['controller' => 'Tags', 'action' => 'mergePreview']);
+		// Merge
+		$routes->connect('/tags/merge', ['controller' => 'Tags', 'action' => 'merge']);
+		$routes->connect('/tags/merge-preview', ['controller' => 'Tags', 'action' => 'mergePreview']);
 
-			$routes->fallbacks(DashedRoute::class);
-		});
-	},
-);
+		$routes->fallbacks(DashedRoute::class);
+	});
+});
